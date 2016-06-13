7bdb1ab7b6c96ab4f63113fcfd749aba14a690ed
hejun <hejun@hejundeimac.local>
2016-06-13 14:07:15 +0800

    初始化

---
+++ HJMusicPlayer/HJMusicPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HJMusicPlayer
+//
+//  Created by hejun on 6/13/16.
+//  Copyright © 2016 hejun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
